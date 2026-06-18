d6992fbbaa710f61aad29552b162878cce0567fe
Quân Nguyễn <quanlh3052004@gmail.com>
2026-06-18 22:57:41 +0700

    start project

---
+++ src/main/java/kuan/fintech/fintech_auth_service/FintechAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuan/fintech/fintech_auth_service/FintechAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
